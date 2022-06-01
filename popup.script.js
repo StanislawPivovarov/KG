6431faf94c5644ce8757c31e99259550b88ec97a
Stanislaw Pivovarov <photovarov@bk.ru>
2022-06-01 14:20:39 +0300

    new commit

--- popup.script.js
+++ popup.script.js
@@ -1,10 +1,13 @@
 const popupLinks = document.querySelectorAll('.popup-link');
 const body = document.querySelector('body');
 const lockPadding = document.querySelectorAll('.lock-padding');
-
+const popupCloseIcon = document.querySelectorAll('.close-popup');
 let unlock = true;
 
 const timeout = 800;
+
+
+//работа с ссылками
 
 if(popupLinks.length > 0){
     for (let index = 0; index <popupLinks.length; index++){
@@ -13,9 +16,25 @@ if(popupLinks.length > 0){
             const popupName = popupLink.getAttribute('href').replace('#', '');
             const currentPopup = document.getElementById(popupName);
             popupOpen(currentPopup);
+            e.preventDefault(); //запрет на перезагрузку страницы
+        });
+    }
+}
+
+
+if (popupCloseIcon.length > 0){
+    for(let index = 0; index < popupCloseIcon.length; index++){
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function(e){
+            popupClose(el.closest('.popup'));
             e.preventDefault();
         });
     }
 }
 
-const popo
+function popupOpen(currentPopup){
+    if(currentPopup && unlock) {
+        
+
+    }
+}
